4f54d9f0b41311fc1901bf6f44ad3a0fb395ca49
matsudamper <let@matsudamper.net>
2022-09-12 02:59:12 +0900

    add Apollo to Client sample

--- sample/client/client/build.gradle.kts
+++ sample/client/client/build.gradle.kts
@@ -1,6 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.apollographql.apollo3.gradle.internal.ApolloDownloadSchemaTask
 
 plugins {
+    id("com.apollographql.apollo3") version "3.5.0"
     kotlin("jvm") version "1.7.10"
 }
 
@@ -12,6 +14,11 @@ repositories {
 }
 
 dependencies {
+    val apolloVersion = "3.5.0"
+    implementation("com.apollographql.apollo3:apollo-runtime:$apolloVersion")
+    implementation("com.apollographql.apollo3:apollo-normalized-cache:$apolloVersion")
+    implementation("com.apollographql.apollo3:apollo-adapters:$apolloVersion")
+
     testImplementation(kotlin("test"))
 }
 
@@ -21,4 +28,31 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+
+tasks.getByName<ApolloDownloadSchemaTask>(
+    "downloadApolloSchema"
+) {
+    endpoint.set("http://localhost/schema")
+    schema.set(file("src/main/graphql/schema.graphqls").canonicalPath)
+}
+
+apollo {
+    schemaFiles.setFrom(
+        listOf(
+            File("src/main/graphql/schema.graphqls"),
+        )
+    )
+
+    generateOptionalOperationVariables.set(false)
+    generateKotlinModels.set(true)
+    packageName.set("net.matsudamper.graphql.client.generated")
+
+    mapScalar("UserId", "kotlin.Long")
+    mapScalar("UserPostContentId", "kotlin.Long")
+    mapScalar("JvmInt", "kotlin.Int")
+    mapScalar("JvmFloat", "kotlin.Float")
+    mapScalar("JvmDouble", "kotlin.Double")
+    mapScalar("Date", "java.util.Date", "com.apollographql.apollo3.adapter.DateAdapter")
 }
